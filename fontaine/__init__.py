ad9f1cf62731f559ebdc4cc71754a7bf7b855f14
Vitaly Volkov <vw@dou.ua>
2013-01-09 18:53:58 +0200

    initial commit for project

---
+++ fontaine/__init__.py
@@ -0,0 +1,1 @@
+from fontaine.fontaine import *
---
+++ fontaine/fontaine.py
@@ -0,0 +1,29 @@
+import argparse
+
+from flinfo import Fonts
+
+
+def usage():
+    parser = argparse.ArgumentParser(description='Output information about fonts in different formats')
+    parser.add_argument('font', metavar='font', type=str, nargs='+')
+    parser.add_argument('--text', action='store_true', help='Output information in plain text')
+    parser.add_argument('--xml', action='store_true', help='Output information into XML')
+    parser.add_argument('--json', action='store_true', help='Output information in JSON')
+    return parser.parse_args()
+
+
+def main(*argv):
+    args = usage()
+
+    fonts = Fonts()
+    for _fontname in args.font:
+        fonts.add_font(_fontname)
+
+    if args.text:
+        fonts.print_plain_text()
+    elif args.xml:
+        fonts.print_xml()
+
+
+if __name__ == '__main__':
+    main()
