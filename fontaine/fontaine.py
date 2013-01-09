41d0d04bf6a44e1162f6da5d05b092f3b7461953
Vitaly Volkov <vw@dou.ua>
2013-01-09 19:04:18 +0200

    License text

--- fontaine/fontaine.py
+++ fontaine/fontaine.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+#
+# fontaine.py
+#
+# Copyright (c) 2013, 
+# Виталий Волков <hash.3g@gmail.com> 
+# Dave Crossland <dave@understandinglimited.com>
+#
+# Released under the GNU General Public License version 3 or later.
+# See accompanying LICENSE.txt file for details.
+
 import argparse
 
 from flinfo import Fonts
