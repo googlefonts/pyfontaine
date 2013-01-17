bc24c74ce3f16efce5b5b4641a99f6563bb3c499
Dave Crossland <dave@lab6.com>
2013-01-16 23:58:16 -0800

    Updating GWF subsets to not include latin range

--- fontaine/charmaps/gwf_arabic.py
+++ fontaine/charmaps/gwf_arabic.py
@@ -6,37 +6,8 @@ class Charmap:
     native_name = u''
 
     def glyphs(self):
-        # latin subset from http://code.google.com/p/googlefontdirectory/source/browse/tools/subset/subset.py
-        glyphs  = [0x2013] # endash
-        glyphs += [0x2014] # emdash
-        glyphs += [0x2018] # quoteleft
-        glyphs += [0x2019] # quoteright
-        glyphs += [0x201A] # quotesinglbase
-        glyphs += [0x201C] # quotedblleft
-        glyphs += [0x201D] # quotedblright
-        glyphs += [0x201E] # quotedblbase
-        glyphs += [0x2022] # bullet
-        glyphs += [0x2039] # guilsinglleft
-        glyphs += [0x203A] # guilsinglright
-        glyphs += range(0x20, 0x7f) # Basic Latin (A-Z, a-z, numbers)
-        glyphs += range(0xa0, 0x100) # Western European symbols and diacritics
-        glyphs += [0x20ac] # Euro
-        glyphs += [0x0152] # OE
-        glyphs += [0x0153] # oe
-        glyphs += [0x003b] # semicolon
-        glyphs += [0x00b7] # periodcentered
-        glyphs += [0x0131] # dotlessi
-        glyphs += [0x02c6] # circumflex
-        glyphs += [0x02da] # ring
-        glyphs += [0x02dc] # tilde
-        glyphs += [0x2074] # foursuperior
-        glyphs += [0x2215] # divison slash
-        glyphs += [0x2044] # fraction slash
-        glyphs += [0xe0ff] # PUA: Font logo
-        glyphs += [0xeffd] # PUA: Font version number
-        glyphs += [0xf000] # PUA: font ppem size indicator: run `ftview -f 1255 10 Ubuntu-Regular.ttf` to see it in action!
-        # arabic
-        glyphs += [0x000D, 0x0020, 0x0621, 0x0627, 0x062D,
+        # arabic subset from http://code.google.com/p/googlefontdirectory/source/browse/tools/subset/subset.py
+        glyphs = [0x000D, 0x0020, 0x0621, 0x0627, 0x062D,
                    0x062F, 0x0631, 0x0633, 0x0635, 0x0637, 0x0639,
                    0x0643, 0x0644, 0x0645, 0x0647, 0x0648, 0x0649,
                    0x0640, 0x066E, 0x066F, 0x0660, 0x0661, 0x0662,
--- fontaine/charmaps/gwf_greek.py
+++ fontaine/charmaps/gwf_greek.py
@@ -6,37 +6,8 @@ class Charmap:
     native_name = u''
 
     def glyphs(self):
-        # latin subset from http://code.google.com/p/googlefontdirectory/source/browse/tools/subset/subset.py
-        glyphs  = [0x2013] # endash
-        glyphs += [0x2014] # emdash
-        glyphs += [0x2018] # quoteleft
-        glyphs += [0x2019] # quoteright
-        glyphs += [0x201A] # quotesinglbase
-        glyphs += [0x201C] # quotedblleft
-        glyphs += [0x201D] # quotedblright
-        glyphs += [0x201E] # quotedblbase
-        glyphs += [0x2022] # bullet
-        glyphs += [0x2039] # guilsinglleft
-        glyphs += [0x203A] # guilsinglright
-        glyphs += range(0x20, 0x7f) # Basic Latin (A-Z, a-z, numbers)
-        glyphs += range(0xa0, 0x100) # Western European symbols and diacritics
-        glyphs += [0x20ac] # Euro
-        glyphs += [0x0152] # OE
-        glyphs += [0x0153] # oe
-        glyphs += [0x003b] # semicolon
-        glyphs += [0x00b7] # periodcentered
-        glyphs += [0x0131] # dotlessi
-        glyphs += [0x02c6] # circumflex
-        glyphs += [0x02da] # ring
-        glyphs += [0x02dc] # tilde
-        glyphs += [0x2074] # foursuperior
-        glyphs += [0x2215] # divison slash
-        glyphs += [0x2044] # fraction slash
-        glyphs += [0xe0ff] # PUA: Font logo
-        glyphs += [0xeffd] # PUA: Font version number
-        glyphs += [0xf000] # PUA: font ppem size indicator: run `ftview -f 1255 10 Ubuntu-Regular.ttf` to see it in action!
-        # greek
-        glyphs += range(0x370, 0x400)
+        # greek subset from http://code.google.com/p/googlefontdirectory/source/browse/tools/subset/subset.py
+        glyphs = range(0x370, 0x400)
         return glyphs
 
 library.register(Charmap)
