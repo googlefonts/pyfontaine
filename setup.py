f5be4c673fb3f588ec1cf366266dc66e9204fe02
xen <m@xen.ru>
2013-06-28 05:15:58 +0300

    Distribution ready

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.rst", 'r') as readme_file:
     readme = readme_file.read()
 
 setup(name='fontaine',
-    version='1.0.4',
+    version='1.0.5',
     description='Fontaine font tool',
     license = "GNU GPL",
     long_description=readme,
