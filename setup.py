bc280f14669e874077a5cd4e590bb53b591c9c25
xen <m@xen.ru>
2013-06-28 05:03:14 +0300

    setup.py fixes

--- setup.py
+++ setup.py
@@ -13,19 +13,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import Extension, setup
+from setuptools.command.test import test as TestCommand
+
+with open("README.rst", 'r') as readme_file:
+    readme = readme_file.read()
+
+with open("LICENSE.txt", 'r') as license_file:
+    license = license_file.read()
 
 setup(name='fontaine',
-    version='1.0.1',
+    version='1.0.3',
     description='Fontaine font tool',
-    long_description = open("README.md").read(),
+    license=license,
+    long_description=readme,
     author='Dave Crossland, Виталий Волков',
     author_email='dave@lab6.com',
     url='https://github.com/davelab6/pyfontaine',
     # more examples here http://docs.python.org/distutils/examples.html#pure-python-distribution-by-package
     packages=['fontaine', 'fontaine.charmaps', 'fontaine.structures',],
-    license = "GNU GPL",
-    requires=[
+    install_requires=[
       'freetype',
       'lxml'
     ],
