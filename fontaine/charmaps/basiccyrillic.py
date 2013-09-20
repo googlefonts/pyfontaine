ba91e44d74fada57dd16b71182c324fdc48f6e51
Dave Crossland <dave@lab6.com>
2013-09-20 13:03:07 +0200

    Update Basic Cyrillic charset

--- fontaine/charmaps/basiccyrillic.py
+++ fontaine/charmaps/basiccyrillic.py
@@ -5,7 +5,12 @@ from fontaine.cmap import library
 class Charmap:
     common_name = u'Basic Cyrillic'
     native_name = u'Кири́ллица'
-    glyphs = xrange(0x0410, 0x044f)
-
+    glyphs  = xrange(0x0400, 0x045f)
+    glyphs += [0x0490,
+               0x0491,
+               0x04B0,
+               0x04B1,
+               0x2116,
+              ]
 
 library.register(Charmap)
