ab602f2fe6ec21a63cfbc13bf4665e7b2c4824cd
hash3g <hash.3g@gmail.com>
2014-02-02 10:05:19 +0200

    When --missing argument passed print only list of unicode values

--- fontaine/builder.py
+++ fontaine/builder.py
@@ -43,7 +43,7 @@ def unicodevalues_asstring(values):
     """ Return string with unicodenames (unless that is disabled) """
     if not os.environ.get('DISABLE_UNAMES'):
         return map(lambda x: '%s' % format(x).strip(), values)
-    return map(lambda x: u'U+%04x %s' % (x, unichr(x)), values)
+    return map(lambda x: u'U+%04x %s' % (x, unichr(x)), sorted(values))
 
 
 extract_firstline = lambda text: \
