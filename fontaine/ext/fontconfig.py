ca633d4ac0f3ed2e3aa5cbd466b82b680216986c
hash3g <hash.3g@gmail.com>
2014-02-07 13:56:00 +0200

    Apply include for fontconfig

--- fontaine/ext/fontconfig.py
+++ fontaine/ext/fontconfig.py
@@ -15,35 +15,39 @@ import re
 # 1328-132f  # Ce-CWa
 
 UNICODE_VALUE_REGEX = re.compile('^(?P<bx>0x)?(?P<begr>[0-9a-f]+)(\-(?!0x)(?P<endr>[0-9a-f]+))?', re.I)
+INCLUDE_REGEX = re.compile('^include ([\w.]+)', re.I | re.U)
 
 
 def cmp(val):
     return bool(re.match('0x[0-9A-Fa-f]+\-0x[0-9A-Fa-f]+', val))
 
 
+dirname = os.path.dirname(fontaine.__file__)
+ORTH_SOURCE_DIR = os.path.join(dirname, 'charmaps', 'fontconfig', 'fc-lang')
+
+
 class Fontconfig:
 
     @staticmethod
     def iterate_orth():
-        dirname = os.path.dirname(fontaine.__file__)
-        dirorth = os.path.join(dirname, 'charmaps', 'fontconfig', 'fc-lang')
-        if not os.path.exists(dirorth):
+        if not os.path.exists(ORTH_SOURCE_DIR):
             return []
 
         result = []
-        for fileorth in os.listdir(dirorth):
+        for fileorth in os.listdir(ORTH_SOURCE_DIR):
             if os.path.splitext(fileorth)[1] == '.orth':
-                result.append(os.path.join(dirorth, fileorth))
+                result.append(os.path.join(ORTH_SOURCE_DIR, fileorth))
         return result
 
-    @classmethod
-    def get_string_glyphlist(cls, filename, content):
+    @staticmethod
+    def get_string_glyphlist(filename, content):
         glyphs = []
 
         fn, ext = os.path.splitext(os.path.basename(filename))
         common_name_regex = re.compile(u'#\s+(\w+)\s*\(%s\)$' % fn, re.I | re.U)
 
         common_name = ''
+        include = ''
 
         for line in content.split('\n'):
             common_name_match = common_name_regex.match(line)
@@ -56,6 +60,19 @@ class Fontconfig:
                 if unicode_match.group('endr'):
                     value = value + '-0x' + unicode_match.group('endr')
                 glyphs.append(value)
+
+            regex = INCLUDE_REGEX.match(line)
+            if regex:
+                include = os.path.join(ORTH_SOURCE_DIR, regex.group(1))
+
+        if include and common_name:
+            with open(include) as fp:
+                content = fp.read()
+            name, ng = Fontconfig.get_string_glyphlist(include, content)
+            if name and ng:
+                glyphs += ng.split(',')
+                common_name += u' + %s' % name
+
         return common_name, ','.join(glyphs)
 
     @staticmethod
