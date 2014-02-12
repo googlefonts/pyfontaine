75ce54f03dd584e7df61440c12de34b5b51eda1f
Dave Crossland <dave@lab6.com>
2014-02-12 14:56:40 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 
 from setuptools import setup
 
-with open("README.rst", 'r') as readme_file:
+with open("README.md", 'r') as readme_file:
     readme = readme_file.read()
 
 setup(name='fontaine',
