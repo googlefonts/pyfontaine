bd25c91ee9dcedcca069858a6590e7a0fc67ba9e
Dave Crossland <dave@lab6.com>
2014-02-13 11:19:57 -0500

    Update polish.py

--- fontaine/charmaps/polish.py
+++ fontaine/charmaps/polish.py
@@ -1,16 +1,5 @@
 # -*- coding: utf-8 -*-
-class CharmapAccents:
-
-    common_name = u'Polish Accents'
-    native_name = u'Polskie Akcenty'
-    key = ord(u'ł')
-    polishAccents = u"ĄĆĘŁŃÓŚŹŻąćęłńóśźż"
-    abbreviation = 'PLK'
-    glyphs = map(ord, polishAccents)
-
-
-class CharmapFull:
-    # use a class name unique in the file
+class Charmap:
     common_name = u'Full Polish Alphabet'
     native_name = u'Pełny Polski Alfabet'
     key = ord(u'Ł')
@@ -18,14 +7,17 @@ class CharmapFull:
     polishAlphabet = u"AĄBCĆDEĘFGHIJKLŁMNŃOÓPRSŚTUWYZŹŻaąbcćdeęfghijklłmnńoóprsśtuwyzźż"
     glyphs = map(ord, polishAlphabet)
 
+# class names must be unique in each file
+class CharmapAccents:
+    common_name = u'Polish Accents'
+    native_name = u'Polskie Akcenty'
+    key = ord(u'ł')
+    polishAccents = u"ĄĆĘŁŃÓŚŹŻąćęłńóśźż"
+    abbreviation = 'PLK'
+    glyphs = map(ord, polishAccents)
 
-# Register of custom charmap class. By default it should contain class
-# named `Charmap`. If this class does not contain that we have to register
-# our class manually. Make sure you import library
-#
-# from fontaine.cmap import library
-
+# Class objects named Charmap are registed automatically.
+# Since we have a 2nd Charmap with another name,
+# we must register this class manually
 from fontaine.cmap import library
-
-library.register(CharmapFull)
 library.register(CharmapAccents)
