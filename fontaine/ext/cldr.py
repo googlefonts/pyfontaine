e2d02b48011caa460919195131cb0f6275fc03eb
hash3g <hash.3g@gmail.com>
2014-03-31 09:08:19 +0300

    CLDR interface

---
+++ fontaine/ext/cldr.py
@@ -0,0 +1,75 @@
+# -*- coding utf-8 -*-
+import os
+import os.path as op
+import re
+try:
+    import ujson as pfjson
+except ImportError:
+    import json as pfjson
+import zipfile
+
+from fontaine.ext.base import BaseExt
+from fontaine.ext.update import get_data_directory, get_from_cache
+
+
+JSON_DATA_URL = 'http://www.unicode.org/Public/cldr/latest/json.zip'
+
+
+def to_ordinal(value):
+    try:
+        return ord(value)
+    except TypeError:
+        pass
+
+
+class Extension(BaseExt):
+
+    extension_name = 'cldr'
+
+    @staticmethod
+    def to_charmap(json, language):
+        # collect all glyphs from json data
+        characters = json['main'][language]['characters']
+        exemplar = characters['exemplarCharacters']
+        auxiliary = characters.get('auxiliary')
+        index = characters.get('index')
+        punctuation = characters.get('punctuation')
+        unicodes = []
+        unicodes += re.sub(r'^\[|\]$', '', exemplar).split()
+        if auxiliary:
+            unicodes += re.sub(r'^\[|\]$', '', auxiliary).split()
+        if index:
+            unicodes += re.sub(r'^\[|\]$', '', index).split()
+        if punctuation:
+            unicodes += re.sub(r'^\[|\]$', '', punctuation).split()
+
+        glyphs = []
+        for uni in unicodes:
+            uni = to_ordinal(uni)
+            if not uni:
+                continue
+            glyphs.append(uni)
+        return glyphs
+
+    @staticmethod
+    def __getcharmaps__():
+        pyfontaine_datadir = get_data_directory()
+        cldr_data_directory = op.join(pyfontaine_datadir, 'cldr')
+        # check that we have cldr directory
+        if not op.exists(cldr_data_directory):
+            # if we do not have it then download new or copy existed cldr data
+            # from package and extract it to destination directory
+            filepath = get_from_cache('json.zip', JSON_DATA_URL)
+            zf = zipfile.ZipFile(filepath)
+            zf.extractall(cldr_data_directory)
+
+        for ch in os.listdir(op.join(cldr_data_directory, 'main')):
+            jsonf = op.join(cldr_data_directory, 'main', ch, 'characters.json')
+            jsondata = pfjson.load(open(jsonf))
+
+            locale, ext = op.splitext(ch)
+
+            yield type('Charmap', (object,),
+                       dict(glyphs=Extension.to_charmap(jsondata, locale),
+                            common_name=locale,
+                            native_name='', abbreviation=locale))
--- fontaine/ext/update.py
+++ fontaine/ext/update.py
@@ -10,6 +10,18 @@ import shutil
 APPDATA_DIRNAME = 'pyfontaine'
 
 
+def get_data_directory():
+    try:
+        #Windows code:
+        d = op.join(os.environ["APPDATA"], APPDATA_DIRNAME)
+    except KeyError:
+        #Linux and Mac code:
+        d = op.join(op.expanduser("~"), ".local", "share", APPDATA_DIRNAME)
+    if not op.exists(d):
+        os.makedirs(d)
+    return d
+
+
 def get_file(name, url):
     r = requests.get(url)
     if r.status_code == 200:
@@ -19,17 +31,11 @@ def get_file(name, url):
 
 
 def get_from_cache(filename, url):
-    try:
-        #Windows code:
-        directory = op.join(os.environ["APPDATA"], APPDATA_DIRNAME)
-    except KeyError:
-        #Linux and Mac code:
-        directory = op.join(op.expanduser("~"), ".local", "share", APPDATA_DIRNAME)
-    if not op.exists(directory):
-        os.makedirs(directory)
+    directory = get_data_directory()
     filepath = op.join(directory, filename)
     if not op.exists(filepath) and not get_file(filepath, url):
-        shutil.copy(op.join(os.path.dirname(__file__), filename), filepath)
+        shutil.copy(op.join(os.path.dirname(__file__), 'data', filename),
+                    filepath)
     return filepath
 
 
