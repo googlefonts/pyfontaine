6c5b2c1510da24fe5783de23fce2f6bc011cac64
Shaun Walbridge <shaun.walbridge@gmail.com>
2017-02-03 13:06:13 -0500

    Explicitly label README as UTF-8
    On some machines (e.g. by default in many Docker images), the locale
    will cause setuptools to assume ASCII text by default. This causes a
    UnicodeDecodeError when trying to parse the README.rst for installation.
    Fix this by explicitly labeling the README as UTF-8 prior to reading it.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 
 from setuptools import setup
 
-with open("README.rst", 'r') as readme_file:
+with open("README.rst", 'r', encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
 from fontaine import VERSION
